f07e91cecc0ba45a0b7dcfa491b5786b317106d5
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-17 01:21:18 -0300

    feat: Creating app medico for implement the models especialidades and dados_medicos, creating routes for medicos

---
+++ medico/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import DadosMedico, Especialidades
+
+admin.site.register(Especialidades)
+admin.site.register(DadosMedico)
---
+++ medico/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from medico.models import Especialidades, DadosMedico, is_medico
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
+def cadastro_medico(request):
+
+    
+    if is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Você ja esta cadastrado como medico')
+        return redirect('/medicos/abir_horario')
+            
+    if request.method == 'GET':
+        especialidades = Especialidades.objects.all()
+        return render(request, 'cadastro_medico.html', {'especialidades': especialidades})
+    elif request.method == 'POST':
+        crm = request.POST.get('crm')
+        nome = request.POST.get('nome')
+        cep = request.POST.get('cep')
+        rua = request.POST.get('rua')
+        bairro = request.POST.get('bairro')
+        numero = request.POST.get('numero')
+        cim = request.FILES.get('cim')
+        rg = request.FILES.get('rg')
+        foto = request.FILES.get('foto')
+        especialidade = request.POST.get('especialidade')
+        descricao = request.POST.get('descricao')
+        valor_consulta = request.POST.get('valor_consulta')
+
+        dados_medico = DadosMedico(
+            crm=crm,
+            nome=nome,
+            cep=cep,
+            rua=rua,
+            bairro=bairro,
+            numero=numero,
+            rg=rg,
+            cedula_identidadde_medica=cim,
+            foto=foto,
+            especialidade_id=especialidade,
+            descricao=descricao,
+            valor_consulta=valor_consulta,
+            user=request.user
+        )
+        dados_medico.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso!')
+        return redirect('/medicos/abir_horario')
+
+        
