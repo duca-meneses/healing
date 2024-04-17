bc66f80e383c01b9bcf5b3002c3ce3a763187b0d
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-17 18:20:51 -0300

    feat: Add route and view for abrir_horario

--- medico/views.py
+++ medico/views.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from medico.models import Especialidades, DadosMedico, is_medico
+from medico.models import Especialidades, DadosMedico, is_medico, DatasAbertas
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -49,4 +50,33 @@ def cadastro_medico(request):
         messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso!')
         return redirect('/medicos/abir_horario')
 
+
+def abrir_horario(request):
+
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Somente médicos podem abir horario de consulta.')
+        return redirect('/usuarios/sair')
+
+    if request.method == 'GET':
+        dados_medicos = DadosMedico.objects.get(user=request.user)
+        datas_abertas = DatasAbertas.objects.filter(user=request.user)
+        return render(request, 'abrir_horario.html', {'dados_medicos':dados_medicos, 'datas_abertas':datas_abertas})
+    elif request.method == 'POST':
+        data = request.POST.get('data')
+
+        data_formatada = datetime.strptime(data, '%Y-%m-%dT%H:%M')
+        if data_formatada <= datetime.now():
+            messages.add_message(request, constants.WARNING, 'A data não pode ser anterior a data atual.')
+            return redirect('/medicos/abrir_horario')
+
+        horario_abrir = DatasAbertas(
+            data=data,
+            user=request.user,
+        )
+        horario_abrir.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Horario cadastrado com sucesso.')
+        return redirect('/medicos/abrir_horario')
+
+        
         
