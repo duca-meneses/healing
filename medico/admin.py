01558f54466a86f47f3434b7a8f50004026fef7a
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-17 23:49:29 -0300

    feat: Add DatasAbertas model at admin

--- medico/admin.py
+++ medico/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import DadosMedico, Especialidades
+from .models import DadosMedico, Especialidades, DatasAbertas
 
 admin.site.register(Especialidades)
 admin.site.register(DadosMedico)
+admin.site.register(DatasAbertas)
