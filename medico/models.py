a2a4c4d152638930fdacd354738fe36bee074582
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-17 23:48:11 -0300

    feat: Add a new property for the DadosMedico

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -28,6 +29,15 @@ class DadosMedico(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+
+    @property
+    def proxima_data(self):
+        proxima_data = DatasAbertas.objects.filter(
+            user=self.user).filter(
+                data__gt=datetime.now()).filter(agendado=False).order_by('data').first()
+
+        return proxima_data
+        
 
 class DatasAbertas(models.Model):
     data = models.DateTimeField()
